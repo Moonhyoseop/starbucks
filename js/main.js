3c16bcd216773c9dd756dae475ffc34eb8140cf2
문효섭 <gytjq0723@naver.com>
2022-11-16 00:33:25 +0900

    스타벅스 클론

--- js/main.js
+++ js/main.js
@@ -18,23 +18,41 @@ searchInputEl.addEventListener('blur', function(){
 
 // 고정 뱃지 스크롤 내리면 사라졌다가 올리면 나타남 
 const badgeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top');
 
 window.addEventListener('scroll', _.throttle(function (){
   console.log(window.scrollY)
   if(window.scrollY > 500){
-   //gsap.to(요소, 지속시간, 옵션)
+    //배지숨기기
+    //gsap.to(요소, 지속시간, 옵션)
    gsap.to(badgeEl, .6, {
     opacity: 0,
     display: 'none'
    });
+   //버튼보이기
+   gsap.to(toTopEl, .2,{
+    x: 0
+   })
   }else{
+    //배지보이기
     gsap.to(badgeEl, .6, {
       opacity: 1,
       display: 'block'
      });
+     //버튼숨기기
+     gsap.to(toTopEl, .2, {
+      x: 100
+     });
   }
 }, 300));
 // _.throttle(함수, 시간)
+
+
+toTopEl.addEventListener('click', function(){
+  gsap.to(window, .7, {
+    scrollTo: 0
+  });
+});
 
 
 const fadeEls = document.querySelectorAll('.visual .fade-in');
@@ -73,6 +91,17 @@ new Swiper('.promotion .swiper-container', {
   }
 });
 
+new Swiper('awards .swiper-container', {
+  autoplay: true,
+  loop: true,
+  spaceBetween: 30,
+  slidesPerView: 5,
+  navigation: {
+    prevEl:'.awards .swiper-prev',
+    nextEl:'.awards .swiper-next'
+  }
+});
+
 const promotionEl = document.querySelector('.promotion');
 const promotionToggleBtn = document.querySelector('.toggle-promotion');
 let isHidePromotion = false;
@@ -87,3 +116,42 @@ promotionToggleBtn.addEventListener('click', function(){
     promotionEl.classList.remove('hide');
   }
 });
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max){
+  //`.toFixed()`를 통해 반환된 문자 데이터를,
+  //`parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min ) + min))
+}
+
+
+function floatingObject(selector, delay, size){
+  //gsap.to(요소, 지속시간, 옵션);
+  gsap.to(selector, 
+    random(1.5, 2.5),
+    {
+    y: size,
+    repeat: -1,
+    yoyo: true,
+    ease: Power1.easeInOut,
+    delay: random(0, delay)
+  });
+}
+
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
+
+const spyEls = document.querySelectorAll('section.scroll-spy');
+spyEls.forEach(function(spyEl){
+  new ScrollMagic
+    .Scene({
+      triggerElement: spyEl,
+      triggerHook: .8 // 보여짐 여부 감시할 요소 지정
+    })
+    .setClassToggle(spyEl, 'show')
+    .addTo(new ScrollMagic.Controller());
+});
+
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();
